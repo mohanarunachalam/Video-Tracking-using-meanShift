da24c383cd1fdaa5688430196f1f66a7a8ce4d0c
student-1 <ran.feldesh@gmail.com>
2017-12-07 16:07:48 +0200

    Adding tracking point

--- FIxedPoint.py
+++ FIxedPoint.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-for i in range(1, 112):
+for i in range(1, 40):
     filename = str(i)
     if len(filename)==1:
         filename = '00' + filename + '.png'
@@ -9,6 +9,9 @@ for i in range(1, 112):
     if len(filename) == 3:
         filename = filename + '.png'
     img = plt.imread(filename)
-    plt.imshow(img)
+    (r1,r2,c1,c2) = (img.shape[0] / 2 - 100, img.shape[0] / 2 - 80,img.shape[1] / 2 - 10, img.shape[1] / 2 + 10)
+    imgTracked = img
+    imgTracked[r1:r2, c1:c2, :] = 0
+    plt.imshow(imgTracked)
     plt.show(block=False)
     plt.pause(0.02)
